f20b49090602159d3c9427960f69542722d8abb1
renatormc <renatomartinsrmc@gmail.com>
2022-04-15 18:07:38 -0300

    Normal

--- rlibs/report_writer/converters.py
+++ rlibs/report_writer/converters.py
@@ -1,7 +1,11 @@
 from datetime import datetime
+from pathlib import Path
 
 def str2date(value: str) -> datetime:
     try:
         return datetime.strptime(value, "%d/%m/%Y")
     except:
         raise Exception(f"{value} não é uma data válida")
+
+def get_pics_from_folder(folder: str | Path):
+    pass
